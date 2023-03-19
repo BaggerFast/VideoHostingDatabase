8e892127ed47dc62ba333a7490a439a07e771a4c
BaggerFast <riosha3@gmail.com>
2023-03-19 20:46:28 +0300

    UPDATE: refactor TAGS scripts

--- VideoHosting/TAGS/TAGS_INSERT.sql
+++ VideoHosting/TAGS/TAGS_INSERT.sql
@@ -6,7 +6,6 @@ SET ANSI_NULLS ON;
 SET QUOTED_IDENTIFIER ON;
 SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
 
-DECLARE @DB_NAME VARCHAR(128) = 'VideoHosting';
-DECLARE @DB_NAME_CURRENT VARCHAR(128) = NULL;
 DECLARE @IS_COMMIT BIT = 0;
 -------------------------------------------------------------------------------------------------
+USE VideoHosting;
