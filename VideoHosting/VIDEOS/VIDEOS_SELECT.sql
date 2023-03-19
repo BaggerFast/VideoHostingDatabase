b1c0cc1de2250b07104b2431bcc65b510def38df
BaggerFast <riosha3@gmail.com>
2023-03-19 21:19:22 +0300

    ADD: VIDEOS scripts

---
+++ VideoHosting/VIDEOS/VIDEOS_SELECT.sql
@@ -0,0 +1,27 @@
+-------------------------------------------------------------------------------------------------
+-- VIDEOS_SELECT
+-------------------------------------------------------------------------------------------------
+SET NOCOUNT ON;
+SET ANSI_NULLS ON;
+SET QUOTED_IDENTIFIER ON;
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+-------------------------------------------------------------------------------------------------
+USE VideoHosting;
+
+IF NOT EXISTS (SELECT 1 FROM [sys].[tables] WHERE [name] = N'VIDEOS') BEGIN
+	PRINT N'❌ TABLE [VIDEOS] WAS NOT FOUND';
+END ELSE BEGIN
+	SELECT
+		[VIDEO].[UID],
+        [VIDEO].[TITLE],
+        [VIDEO].[DESCRIPTION],
+        [USERS].[USERNAME] [USER],
+        [AGE_REST].[AGE] [AGE_RESTRICTIONS],
+        [ACCESS].[TITLE] [ACCESS],
+        [VIDEO].[CREATED_DT] [REGISTER_DT]
+	FROM [VIDEOS] [VIDEO]
+    LEFT JOIN [USERS] [USERS] ON [VIDEO].[USER_UID]=[USERS].[UID]
+	LEFT JOIN [AGE_RESTRINCTIONS] [AGE_REST] ON [VIDEO].[AGE_RESTRICTIONS_UID]=[AGE_REST].[UID]
+    LEFT JOIN [ACCESS_LEVELS] [ACCESS] ON [VIDEO].[ACCESS_UID]=[ACCESS].[UID]
+	ORDER BY [VIDEO].[CREATED_DT];
+END;
---
+++ VideoHosting/VIDEOS/VIDEOS_TRUNC.sql
@@ -0,0 +1,29 @@
+-------------------------------------------------------------------------------------------------
+-- VIDEOS_TRUNC
+-------------------------------------------------------------------------------------------------
+SET NOCOUNT ON;
+SET ANSI_NULLS ON;
+SET QUOTED_IDENTIFIER ON;
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
+DECLARE @IS_COMMIT BIT = 0;
+-------------------------------------------------------------------------------------------------
+USE VideoHosting;
+
+BEGIN TRAN
+PRINT N'➕ JOB IS STARTED';
+
+IF NOT EXISTS (SELECT 1 FROM [sys].[tables] WHERE [name] = N'VIDEOS') BEGIN
+	PRINT N'❌ TABLE [VIDEOS] WAS NOT FOUND';
+END ELSE BEGIN
+	TRUNCATE TABLE [VIDEOS];
+	PRINT N'➕ TRUNCATE TABLE [VIDEOS] IS COMPLETED';
+END;
+
+IF (@IS_COMMIT = 1) BEGIN
+    COMMIT TRAN
+    PRINT N'➕ INSERT IS COMMITTED'
+END ELSE BEGIN
+    ROLLBACK TRAN
+    PRINT N'❌ INSERT IS ROLL-BACKED'
+END;
