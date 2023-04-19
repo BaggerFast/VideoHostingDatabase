b1e681b9e20347ac41996a07969dfbe08a81df05
BaggerFast <riosha3@gmail.com>
2023-04-19 21:37:20 +0300

    MOVE: SqlScripts to Tables directory

---
+++ VideoHosting/MISC/EXAMPLES.sql
@@ -0,0 +1,11 @@
+USE VideoHosting;
+
+--SELECT dbo.GET_USER_FOLLOWERS_COUNT(N'BaggerFast') AS [USER'S FOLLOWERS];
+
+--SELECT dbo.GET_VIDEO_REACTION_PERCENT(N'Деплой TELEGRAM БОТА на удаленный Linux сервер | VPS & UBUNTU') AS [REACTION %];
+
+--EXEC dbo.GET_VIDEO_FOR_USER @USER_NAME = N'Gogus33'
+
+--EXEC dbo.GET_VIDEO_BY_TAG @TAG_TITLE = N'Python'
+
+--EXEC dbo.SET_TAG_TO_VIDEO @VIDEO_TITLE = N'Деплой TELEGRAM БОТА на удаленный Linux сервер | VPS & UBUNTU', @TAG_TITLE = N'ChatGptqqd'
